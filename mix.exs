f1aba6239ccf04599f5c4b1b61f3eeda253ee992
Raphael D. Pinheiro <raphaklaus@gmail.com>
2020-08-22 15:11:47 +0200

    Add coveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule SunlightHours.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
     ]
   end
 
@@ -21,7 +23,8 @@ defmodule SunlightHours.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 end
